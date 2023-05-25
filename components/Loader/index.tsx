bdde78f544059c4a04253633cd54e3c9758c3f7c
Muhammad Musaddiq <46813791+msuaddiq@users.noreply.github.com>
2023-05-25 19:39:09 +0500

    fix: fixed loader component code

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -1,11 +1,11 @@
-import { Box, CircularProgress } from "@mui/material"
+import { Box, CircularProgress } from "@mui/material";
 
 const Loader: React.FC = () => {
-    return (
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <CircularProgress />
-        </Box>
-    )
-}
+  return (
+    <Box sx={{display: "flex",alignItems: "center",justifyContent: "center",height: "100vh",}}>
+      <CircularProgress />
+    </Box>
+  );
+};
 
-export default Loader
+export default Loader;
