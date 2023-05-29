8747031d1d46a66aa1c2372df7aeea2e669eb111
Muhammad Musaddiq <46813791+msuaddiq@users.noreply.github.com>
2023-05-29 19:11:15 +0500

    fix: added changes component

--- screens/Teacher/ManageQuizScreen/tableSection.tsx
+++ screens/Teacher/ManageQuizScreen/tableSection.tsx
@@ -6,8 +6,20 @@ import {
   pageSizeManageQuiz,
   rowsManageQuiz,
 } from "mock-data/Teacher/ManageQuiz";
+import { Box } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 
 const TableSection = () => {
+  const config = {
+    key: "addStudents",
+    startIcon: <ArrowCircleRightOutlinedIcon />,
+    render: () => {
+      return <Box>Create New</Box>;
+    },
+    onClick: () => {
+      // console.log("Add Students");
+    },
+  };
   return (
     <BoxWrapper>
       <DataGridWithPagination
