90f51a0e087c9e1c9c4b8fba407f4e44ff14ad1a
Yarpa Mehdi <yarpamehdi2000@gmail.com>
2023-05-30 16:50:01 +0500

    feat: student layout created

---
+++ pages/teacher/students/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import ManageStudents from "screens/Teacher/ManageStudent/manage";
+
+const StudentManage: NextPage = () => {
+  return <ManageStudents />;
+};
+
+export default StudentManage;
---
+++ screens/Teacher/ManageStudent/manageStudents.jsx
@@ -0,0 +1,15 @@
+import PageLayout from 'components/PageLayout'
+import React from 'react'
+
+
+const ManageStudents = () => {
+  return (
+    <>
+        <PageLayout>
+            <h1>hello World</h1>
+        </PageLayout>
+    </>
+  )
+}
+
+export default ManageStudents
