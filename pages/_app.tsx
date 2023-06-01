b98948f0fa26d1b343a6d5d03070286cc01e620b
Muhammad Musaddiq <46813791+msuaddiq@users.noreply.github.com>
2023-06-01 21:01:29 +0500

    fix: conflict resolved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,13 @@ import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Hydrate } from "react-query/hydration";
 
+import { AppStateProvider } from "contexts/AppStateContext";
 import { AuthContextProvider } from "contexts/AuthContext";
 import ThemeContextProvider from "contexts/ThemeContext";
 import { getLocale, getMessages } from "i18n";
 import { initFirebase } from "platform/initFirebase";
 import ThemeProvider from "theme/Provider";
 import { initAnalytics } from "../platform/analytics";
-import { AppStateProvider } from "contexts/AppStateContext";
 import "react-quill/dist/quill.snow.css";
 
 const loadSideEffects = () => {
