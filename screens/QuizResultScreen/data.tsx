44bd3d6a0fb991b9c3883578989ebd3c6ad9f4fc
MubeenQazi <mubeenqazi32@gmail.com>
2023-06-02 08:00:36 -0700

    feat: added help screen

--- screens/QuizResultScreen/data.tsx
+++ screens/QuizResultScreen/data.tsx
@@ -14,3 +14,22 @@ export const selectStatusOption = [
   { id: 0, name: "Completed" },
   { id: 1, name: "Draft" },
 ];
+
+export const helpContent = [
+  {
+    title:'Filters:',
+    description:'Use the course and folder drop down menus to filter your completed and partially completed quizzes.'
+  },
+  {
+    title:'Difficulty:',
+    description:'The difficulty is simply the number of times the question has been shown divided by the number of attempt all users have made to determine the correct answer.'
+  },
+  {
+    title:'Std. Difficulty:',
+    description:'This is the more traditional difficulty score, the number of error free responses to a particular quiz.'
+  },
+  {
+    title:'Quiz #',
+    description:'Click on the quiz number that you want to see student and question level scoring for a particular quiz.'
+  }
+]
--- screens/QuizResultScreen/index.tsx
+++ screens/QuizResultScreen/index.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import PageLayout from "components/PageLayout";
+import React, { useState } from "react";
 import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
+import { Tooltip } from 'react-tooltip'
+
+import HelpModal from "components/HelpPage";
+import PageLayout from "components/PageLayout";
+import FormattedMessage, { useFormattedMessage } from "theme/FormattedMessage";
+
 import QuizesListing from "./QuizesListing";
+import messages from "./messages";
+import { helpContent } from "./data";
 
 const QuizResultScreen = () => {
+  const [open, setOpen] = useState(false)
+  const modalTitle = useFormattedMessage(messages.helpTitle)
   return (
-    <PageLayout title="Quiz Results" icon={<HelpRoundedIcon />}>
-      <QuizesListing />
+    <PageLayout title="Quiz Results" icon={ <HelpRoundedIcon /> } onIconClick={() => setOpen(true)}>
+      <>
+        <QuizesListing />
+        <HelpModal isOpen={open} onClose={() => setOpen(false)} title={modalTitle} heading={<FormattedMessage {...messages.helpHeading} />} description={<FormattedMessage {...messages.helpDescription} />} content={helpContent} />
+      </>
     </PageLayout>
   );
 };
