acf75ab28aa03f1fc29b2381f0a6508d81ae7e8e
Shahroz Ahmed <shahrozahmed286@gmail.com>
2023-06-02 14:14:41 +0500

    fix: in progress work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
+import React, { useEffect } from "react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import HomeScreen from "screens/HomeScreen";
+import APP_ROUTES from "constants/RouteConstants";
 // import Button from "theme/Button";
-
 // import Head from "next/head";
 // import Image from "next/image";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  // Redirect to '/dashboard' when the page loads
+
+  useEffect(() => {
+    router.push(APP_ROUTES.DASHBOARD);
+  }, []);
+
   return (
     <>
       <HomeScreen />
