68ced4aae89725550f6dd2cabeb276267beea8af
MubeenQazi <mubeenqazi32@gmail.com>
2023-06-05 08:56:14 -0700

    fix: fixed folder structure

--- pages/student/test-yourself/index.tsx
+++ pages/student/test-yourself/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import TestYourself from "screens/StudentScreen/TestYourself";
+import TestYourself from "screens/Student/TestYourself";
 
 const QuestionCreate: NextPage = () => {
   return <TestYourself />;
