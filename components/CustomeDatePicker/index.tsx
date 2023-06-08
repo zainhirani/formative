13533a473cd9329535f8847e4dfcaed266cf1fbe
Shahroz Ahmed <shahrozahmed286@gmail.com>
2023-06-08 11:20:51 +0500

    fix: fixes

--- components/CustomeDatePicker/index.tsx
+++ components/CustomeDatePicker/index.tsx
@@ -1,35 +1,33 @@
-import { useState,FC } from "react";
+import { FC, useState } from "react";
 
 import { TextField } from "@mui/material";
-import {
-  SelectBoxWrapper,
-} from "./Styled";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { SelectBoxWrapper } from "./Styled";
 
 type ICustomeDatePicker = {
-label:string | React.ReactElement,
-value?: null | Date;
-onChange?: (e?:any) => void
+  label: string | React.ReactElement;
+  value?: null | Date;
+  onChange?: (e?: any) => void;
+};
 
-}
-
-
-
-const CustomeDatePicker:FC<ICustomeDatePicker> = ({label,value,onChange}) => {
-
+const CustomeDatePicker: FC<ICustomeDatePicker> = ({
+  label,
+  value,
+  onChange,
+}) => {
   return (
-      <SelectBoxWrapper  gridColumn="span 3">
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker 
-            className="date_picker"
-            label={label}
-            value={value}
-            onChange={onChange}
-          />
-        </LocalizationProvider>
-      </SelectBoxWrapper> 
+    <SelectBoxWrapper gridColumn="span 3">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          className="date_picker"
+          label={label}
+          value={value}
+          onChange={onChange}
+        />
+      </LocalizationProvider>
+    </SelectBoxWrapper>
   );
 };
 
