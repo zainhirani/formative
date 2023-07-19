5bef647e35dbd583ca3f4e44cde8a118bd735494
Muhammad Musaddiq <46813791+msuaddiq@users.noreply.github.com>
2023-07-19 19:30:10 +0500

    fix: build issue fixed

--- components/DrawerCreateQuestionsSection/index.tsx
+++ components/DrawerCreateQuestionsSection/index.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SideDrawer from "components/Drawer";
 
 const DrawerQuestionsSection = (props: any) => {
